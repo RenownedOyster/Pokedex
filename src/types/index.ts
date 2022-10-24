9ea9f3b24a43bdaa37c06b796320620fde29ff5b
Tony Butler <anthonywbutler1@gmail.com>
2022-10-24 16:21:37 +0200

    feat: add type index export

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+import {
+  AllPokemonResponse,
+  AllPokemonResponseResults,
+  PokemonAxiosResponse,
+} from "./pokemon";
+
+import {
+  IndividualPokemonResponse,
+  Characteristics,
+  Stat,
+  Ability,
+  Type,
+} from "./individualPokemonResponse";
+
+export type {
+  AllPokemonResponse,
+  AllPokemonResponseResults,
+  PokemonAxiosResponse,
+  IndividualPokemonResponse,
+  Characteristics,
+  Stat,
+  Ability,
+  Type,
+};
